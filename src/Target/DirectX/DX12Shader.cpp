288060aa0f460c920bd2104cef6435116c6371a1
z8z6 <zhou_zhengming@foxmail.com>
2026-07-26 20:23:19 +0800

    [dev] refractor dxc

--- src/Target/DirectX/DX12Shader.cpp
+++ src/Target/DirectX/DX12Shader.cpp
@@ -5,6 +5,7 @@
 #include "Util/Error.h"
 #include <d3dcompiler.h>
 #include <dxcapi.h>
+#include <iostream>
 
 using namespace z8;
 
@@ -29,71 +30,84 @@ void DX12Shader::CompileByFxc()
 }
 
 void DX12Shader::CompileByDxc() {
-  // 1. 创建 DXC 编译器实例
-  ComPtr<IDxcUtils> dxcUtils;
-  ComPtr<IDxcCompiler3> dxcCompiler;
-  HRESULT hr;
+  ComPtr<IDxcUtils> utils;
+  ComPtr<IDxcCompiler3> compiler;
+  ComPtr<IDxcIncludeHandler> includeHandler;
 
-  hr = DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&dxcUtils));
-  hr = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&dxcCompiler));
+  // 创建 DXC 组件
+  Ok(DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&utils)));
+  Ok(DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&compiler)));
+  Ok(utils->CreateDefaultIncludeHandler(&includeHandler));
 
-  // 2. 读取着色器文件
-  ComPtr<IDxcBlobEncoding> shaderSource;
-  hr = dxcUtils->LoadFile(S->FileName.c_str(), nullptr, &shaderSource);
-  if (FAILED(hr))
-  {
-    OutputDebugStringA("Failed to load shader file!\n");
-    Ok(hr);
-    return;
+  // 读取文件内容
+  ComPtr<IDxcBlobEncoding> sourceBlob;
+  Ok(utils->LoadFile(S->FileName.c_str(), nullptr, &sourceBlob));
+
+  // 准备 DxcBuffer
+  DxcBuffer sourceBuffer;
+  sourceBuffer.Ptr = sourceBlob->GetBufferPointer();
+  sourceBuffer.Size = sourceBlob->GetBufferSize();
+  sourceBuffer.Encoding = DXC_CP_ACP;  // 使用 ANSI 编码
+
+  // 准备编译参数
+  // dxc.exe -E VS -T vs_6_0 .\Default.hlsl -Fo vs.cso
+  // dxc.exe -E PS -T ps_6_0 .\Default.hlsl -Fo ps.cso
+  std::wstring entryW = std::wstring(S->Entry.begin(), S->Entry.end());
+  std::wstring targetW = std::wstring(S->Target.begin(), S->Target.end());
+
+  std::vector<LPCWSTR> arguments;
+  arguments.push_back(L"-E");
+  arguments.push_back(entryW.c_str());
+  arguments.push_back(L"-T");
+  arguments.push_back(targetW.c_str());
+
+  std::wcout << (L"Arguments:\n");
+  for (auto& arg : arguments) {
+    std::wcout <<(arg);
+    std::wcout <<(L" ");
   }
+  std::wcout <<(L"\n");
+  return;
 
-  // 3. 编译参数配置
-  DxcBuffer sourceBuffer{};
-  sourceBuffer.Ptr = shaderSource->GetBufferPointer();
-  sourceBuffer.Size = shaderSource->GetBufferSize();
-  sourceBuffer.Encoding = DXC_CP_UTF8;
-
-  // 编译参数列表
-  std::vector<LPCSTR> arguments;
-
-  // 入口函数 + 着色器目标 (vs_6_0 / ps_6_0 等)
-  arguments.push_back("-E");
-  arguments.push_back(S->Entry.c_str());  // ANSI 转宽字符
-  arguments.push_back("-T");
-  arguments.push_back(S->Target.c_str()); // 支持 vs_6_0、ps_6_0、cs_6_0
-
-  // Debug 模式：开启调试信息 + 关闭优化
-#if defined(DEBUG) || defined(_DEBUG)
-  arguments.push_back("-Zi");     // 调试信息
-  arguments.push_back("-Od");     // 关闭优化
-  arguments.push_back("-Qembed_debug"); // 嵌入调试信息到着色器
-#endif
-
-  // 4. 执行编译
+  // 编译
   ComPtr<IDxcResult> compileResult;
-  // hr = dxcCompiler->Compile(
-  //     &sourceBuffer,
-  //     arguments.data(),
-  //     (UINT32)arguments.size(),
-  //     nullptr,
-  //     IID_PPV_ARGS(&compileResult)
-  // );
-  Ok(hr);
+  Ok(compiler->Compile(
+      &sourceBuffer,
+      arguments.data(),
+      static_cast<uint32_t>(arguments.size()),
+      includeHandler.Get(),
+      IID_PPV_ARGS(&compileResult)
+  ));
 
-  // 5. 输出编译错误
-  ComPtr<IDxcBlobUtf8> errors;
-  compileResult->GetOutput(DXC_OUT_ERRORS, IID_PPV_ARGS(&errors), nullptr);
-  if (errors != nullptr && errors->GetStringLength() > 0)
+  // 检查编译是否成功
+  HRESULT status;
+  Ok(compileResult->GetStatus(&status));
+  Ok(status);
+
+  // 获取编译结果
+  ComPtr<IDxcBlobUtf16> err;
+  Ok(compileResult->GetOutput(DXC_OUT_ERRORS, IID_PPV_ARGS(&err), nullptr));
+
+  if (err && err->GetStringLength() > 0)
   {
-    OutputDebugStringA((char*)errors->GetBufferPointer());
+    // 输出错误信息
+    std::wstring errorMsg(err->GetStringPointer());
+    OutputDebugStringW(errorMsg.c_str());
+
+    // 也可以转换为 ANSI 输出
+    int len = WideCharToMultiByte(CP_ACP, 0, errorMsg.c_str(), -1, nullptr, 0, nullptr, nullptr);
+    std::string ansiError(len, 0);
+    WideCharToMultiByte(CP_ACP, 0, errorMsg.c_str(), -1, &ansiError[0], len, nullptr, nullptr);
+    OutputDebugStringA(ansiError.c_str());
   }
 
-  // 6. 获取编译成功的着色器字节码
-  compileResult->GetOutput(DXC_OUT_OBJECT, IID_PPV_ARGS(&ByteCode), nullptr);
 
-  // // 7. 检查最终结果
-  compileResult->GetStatus(&hr);
-  Ok(hr);
+  if (SUCCEEDED(status)){
+      // 获取字节码
+      compileResult->GetOutput(DXC_OUT_OBJECT, IID_PPV_ARGS(&ByteCode), nullptr);
+  }
+
+
 }
 
 D3D12_SHADER_BYTECODE DX12Shader::GetByteCode() const
@@ -105,6 +119,7 @@ D3D12_SHADER_BYTECODE DX12Shader::GetByteCode() const
 void DX12ShaderRegistry::Register(Shader* s) {
   auto* B = new DX12Shader(s);
   B->CompileByFxc();
+  //B->CompileByDxc();
   s->Binary = B;
   Shaders[s->Name] = s;
 }
